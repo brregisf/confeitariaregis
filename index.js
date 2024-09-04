fd41a5d638f620d4a0d2252ffa3df32ff428d0e7
brregisf <lucasregisf@icloud.com>
2024-09-04 06:20:59 -0300

    0.60.01

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ ScrollReveal().reveal(".media", {distance: 0, opacity: 0, delay: 150})
 
 ScrollReveal().reveal(".info", {distance: '20px'})
 ScrollReveal().reveal(".info", {delay: 200})
+
+ScrollReveal().reveal(".ad1", {distance: '15px', delay: 150, opacity: 0})
